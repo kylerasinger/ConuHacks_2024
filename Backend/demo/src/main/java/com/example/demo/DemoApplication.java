87efb3d8a1110cb4081a98d23fbe57ff84753888
khaled Elshokri <khaledelshokri28@outlook.com>
2024-01-20 19:40:46 -0500

    added controller

--- Backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,15 @@
 package com.example.demo;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping
-	public String hello() {
-		return "hello";
-	}
+	
 
 }
