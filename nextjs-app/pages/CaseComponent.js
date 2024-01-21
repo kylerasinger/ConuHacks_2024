414fe306ca6b909467058784108ca823a9fcaeb1
Captaintom14 <60295341+Captaintom14@users.noreply.github.com>
2024-01-21 04:32:42 -0500

    center the brand

---
+++ nextjs-app/pages/CaseComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+const Case = () => {
+
+return (
+
+<div className = {styles.myCase}>
+    <div className = {styles.innerCase}>
+     <p className = {styles.caseText}> Meet the Team </p>
+     
+     </div>
+     </div>
+    
+);
+
+};
+
+export default Case; 
